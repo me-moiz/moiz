cf882eb846b03d1977c34fe6656038bbda06c9c4
Syed Moiz <syedamahnoorw@gmail.com>
2024-11-20 16:29:21 +0500

    Add files via upload

---
+++ moiz.php
@@ -0,0 +1,93 @@
+<?php
+
+
+// $num  = 10;
+// $num_02 = "10";
+
+// airthmrtic operator
+// echo($num + $num_02);
+// echo($num - $num_02);
+// echo($num * $num_02);
+// echo($num / $num_02);
+// echo($num % $num_02);
+
+//assignment operator
+
+// $num =-5 ;
+// $num =+59 ;
+// echo($num);
+
+// comparison operator
+
+// echo ($num != $num_02);
+// echo ($num === $num_02);
+// echo ($num >= $num_02);
+//  echo ($num !== $num_02);
+
+// $admin = "MOIZ";
+// $pass = 246810;
+// $admin_02 = "SAMAD";
+// $pass_02 = 12345;
+
+// if ($admin == "MOIZ" && $pass == 246810 || $admin_02 == "SAMAD" && $pass_02 == 12345) {
+//     echo ("Welcome Admin");
+// } else {
+//     echo ("Login Filed");
+// }
+
+// switch {
+//     case "";
+//     break;
+
+//     default;
+// }
+
+//echo("PHP Programming");
+//echo ("<h1>PHP Programming</h1>");
+// $show = "PHP Programming";
+//echo("I am Leaning " . $show);
+// echo("<script>alert('php programming');</script>");
+// echo("10");
+// echo(10);
+// $num = 10;
+// $num_02 = 100;
+// echo($num > $num_02)
+
+// $admin = "MOIZ";
+// $pass = 12345;
+// $admin_02 = "SAMAD";
+// $pass_02 = 12346;
+
+// switch ($admin == "MOIZ" && $pass == 12345 || $admin_02 == "SAMAD" && $pass_02 == 123456) {
+//     case true:
+//         echo("Welcome");
+//         break;
+
+//     default:
+//     echo("Login Denied");
+//         break;
+// }
+
+// for ($i = 0; $i < 11; $i++) {
+//     if ($i == 5) {
+//         continue;
+//     }
+//     echo ("<h1>This is For Loop</h1>".$i ." <br>");
+// }
+
+// for ($i = 1; $i <= 5; $i++) {
+//     for ($j = 1; $j <= 3; $j++) {
+//         echo ("ye i hai " . $i . " ye j h  " . $j . "<br>");
+//     }
+
+// }
+
+// $a = 0;
+// while ($a <= 10) {
+//     if ($a = 5) {
+//      break;
+//     }
+//     echo ("<h1>This is While Loop</h1>". $a. "<br>");
+//     $a++;
+// }
+?>
